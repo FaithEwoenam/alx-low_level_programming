42b9fb1e6de29a5f4049b7f070d1fcba5918a856
FaithEwoenam <faith.ewoenam.azah@gmail.com>
2023-09-14 14:27:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char *program_name = strrchr(argv[0], '/');
+    if (program_name == NULL) {
+        program_name = argv[0];
+    } else {
+        program_name++;
+    }
+    printf("%s\n", program_name);
+    return 0;
+}
+
